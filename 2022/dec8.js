3c8435563b97c833f3d201a456f92539b5891a2a
Brynn Smith <brynn@genesisbox.ca>
2022-12-07 23:26:38 -0500

    December 8th Challenge

---
+++ 2022/dec8.js
@@ -0,0 +1,51 @@
+function validTime(str) {
+    return str.split(':')[0] >= 0 && str.split(':')[0] <= 23
+            && str.split(':')[1] >= 0 && str.split(':')[1] <= 59
+}
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
