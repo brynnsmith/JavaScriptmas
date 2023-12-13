6477441ee9a84658e198f49220fb5d64b128aec7
Brynn Smith <brynn@genesisbox.ca>
2023-12-12 21:46:55 -0500

    Solved day12

---
+++ 2023/day12-santasGiftSorter.js
@@ -0,0 +1,14 @@
+const xmasGifts = ['guitar 🎸', 'skates ⛸️', 'bear 🧸', 'magnet 🧲', 'laptop 💻', 'games console 🎮 ', 'jewellery 💍', 'kite 🪁']
+
+/**
+ * Challenge:
+ * 1. Sort the array twice. Alphabetically and reverse alphabetically.  
+ **/
+
+const sortedAZ = xmasGifts.sort((a, b) => a > b);
+console.log('A-Z: ', sortedAZ) 
+//["bear 🧸", "games console 🎮 ", "guitar 🎸", "jewellery 💍", "kite 🪁", "laptop 💻", "scarf 🧣", "skates ⛸️"]
+
+const sortedZA = xmasGifts.sort((a, b) => a < b);
+console.log('Z-A: ', sortedZA) 
+//["skates ⛸️", "scarf 🧣", "laptop 💻", "kite 🪁", "jewellery 💍", "guitar 🎸", "games console 🎮 ", "bear 🧸"]
